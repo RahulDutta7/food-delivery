9e126d8722a5dd1219a252282e289701d70dbd77
TECKAT INDIA <inkushwahasonu@gmail.com>
2025-07-02 17:47:31 +0530

    home banner  updated

--- src/components/homeComponent/HomeBanner.tsx
+++ src/components/homeComponent/HomeBanner.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import "./HomeBanner.scss"
-const HomeBanner:React.FC = () => {
+import React from "react";
+import "./HomeBanner.scss";
+const HomeBanner: React.FC = () => {
   return (
     <>
-    <div className='home_banner'>
-    <div className="bg_layout"></div>
-    <div className="banner_content">
-        <div className="left_content">
+      <div className="home_banner">
+        <div className="bg_layout"></div>
+        <div className="banner_content">
+          <div className="left_content">
             <div className="title">
-                Celebrate Love at<br/> GourmentGrove
+              Celebrate Love at
+              <br /> GovernmentGrove
             </div>
             <div className="text">
-                Treat your special someone<br/> to our exclusive Valentine's<br/> menu
+              Treat your special someone
+              <br /> to our exclusive Valentine's
+              <br /> menu
             </div>
+          </div>
+          <div className="right_content">
+            <div className="cart_button">Your Profile</div>
+          </div>
         </div>
-        <div className="right_content">
-            <div className="cart_button">Go to Cart</div>
-        </div>
-    </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
